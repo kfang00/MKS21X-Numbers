bbe075f1c295342074dab6d8b7af54b37620b081
Kayla Fang <kfang00@stuy.edu>
2018-10-16 02:16:52 -0400

    setup for rational

---
+++ RationalNumber.java
@@ -0,0 +1,16 @@
+public class RationalNumber extends RealNumber{
+	private int num;
+	private int den;
+
+	public RationalNumber() {
+		super(15);
+	}
+
+	public RationalNumber(int n, int d) {
+		super(13.0);
+		num = n;
+		den = d;
+	}
+	
+
+}
