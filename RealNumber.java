da693bd2a50d9ce6a25ef5b937988dcfead673c8
Kayla Fang <kfang00@stuy.edu>
2018-10-16 02:10:37 -0400

    setup and methods

---
+++ RealNumber.java
@@ -0,0 +1,19 @@
+public class RealNumber{
+	private double x;
+
+	public RealNumber() {
+		x = 10;
+	}
+
+	public RealNumber(double x2) {
+		x = x2;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public String toString() {
+		return 
+
+}
