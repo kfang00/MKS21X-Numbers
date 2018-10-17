e82e41dc0054d4f044483e70fd498a4973f292ca
Kayla Fang <kfang00@stuy.edu>
2018-10-17 01:16:37 -0400

    new basic format

--- RealNumber.java
+++ RealNumber.java
@@ -1,20 +1,46 @@
 public class RealNumber{
-	private double x;
+  	private double value;
 
-	public RealNumber() {
-		x = 10;
-	}
+  	public RealNumber(double v){
+    		value = v;
+  	}
 
-	public RealNumber(double x2) {
-		x = x2;
-	}
+  	/*
+  	*Return the sum of this and the other
+  	*/
+  	public double add(RealNumber other){
+     		//other can be ANY RealNumber, including a RationalNumber
+     		//or other subclasses of RealNumber (that aren't written yet)
+     		return 0;
+  	}
 
-	public double getX() {
-		return x;
-	}
+  	/*
+  	*Return the product of this and the other
+  	*/
+  	public double multiply(RealNumber other){
+   		return 0;
+  	}
 
-	public String toString() {
-		return "";
-	}
+  	/*
+  	*Return the this divided by the other
+  	*/
+  	public double divide(RealNumber other){
+    		return 0;
+  	}
+ 
+  	/*
+  	*Return the this minus the other
+  	*/
+  	public double subtract(RealNumber other){
+    		return 0;
+  	}
 
+
+  	public double getValue(){
+    		return value;
+  	}
+
+  	public String toString(){
+    		return ""+value;
+  	}
 }
